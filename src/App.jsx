8fc998293c517c3664a5aebb8d9e06ccbdf9d87e
Sora_G <vgasora@gmail.com>
2026-08-15 20:29:48 +0200

    Import Analytics from @vercel/analytics/react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import { BrowserRouter, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import { Analytics } from "@vercel/analytics/react"
+
 
 
 // Páginas Públicas
@@ -9,7 +9,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import ForgotPassword from './pages/ForgotPassword';
 import Contacto from './pages/public/Contacto';
-
+import { Analytics } from "@vercel/analytics/react"
 // Páginas del Alumno
 import Dashboard from './pages/Dashboard';
 import MisModulos from './pages/MisModulos';
